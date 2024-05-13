bf768fa20c42b750ed2b0c3f71638a01f25c8424
Thijs Dregmans <tdregmans+localGit@protonmail.com>
2024-05-13 16:47:20 +0200

    Add given files from # source: https://wiztech.nl/hr/ti/tinlab_ml/progs/music

---
+++ opdracht2/muser.py
@@ -0,0 +1,36 @@
+# source: https://wiztech.nl/hr/ti/tinlab_ml/progs/music
+
+import glob as gl
+import os
+import tomita.legacy.pysynth as ps
+
+import shutil as su
+
+class Muser:
+    def generate (self, song):
+        songFileName = 'song.wav'
+
+        for trackIndex, track in enumerate (song):
+            ps.make_wav (
+                track,
+                bpm = 130,
+                transpose = 1,
+                pause = 0.1,
+                boost = 1.15,
+                repeat = 0,
+                fn = self.getTrackFileName (trackIndex),
+            )
+
+        if len (song) > 1:
+            ps.mix_files (
+                *[self.getTrackFileName (trackIndex) for trackIndex in range (len (song))],
+                songFileName
+            )
+        else:
+            su.copy ('track_000.wav', songFileName)
+
+        for fileName in gl.glob ('track_*.wav'):
+            os.remove (fileName)
+
+    def getTrackFileName (self, trackIndex):
+        return f'track_{str (1000 + trackIndex) [1:]}.wav'
