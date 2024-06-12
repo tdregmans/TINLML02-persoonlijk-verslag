4b927871b1a91eff7d8971bd1993ddbe48e00d92
Thijs Dregmans <tdregmans+localGit@protonmail.com>
2024-06-12 16:57:29 +0200

    version 6.0

--- opdracht1/main.py
+++ opdracht1/main.py
@@ -5,33 +5,19 @@
     https://github.com/tdregmans/TINLML02-persoonlijk-verslag
 
     main.py
-    Last edited: 2024-04-20 (YYYY-MM-DD)
-    Version: 3.1
+    Last edited: 2024-06-12 (YYYY-MM-DD)
+    Version: 6.0
 
 """
-
-import random
 
 import data
 import NeuralNetwork
 
-# constants
-TESTING_STRICTNESS = 0.51
-TRAINING_STRICTNESS = 0.60
+nn = NeuralNetwork.NeuralNetwork(data.inputDim, data.hiddenDim, data.outputDim)
+nn.trainNetwork(data.trainingSet, noOfEpochs=5000, learningRate=0.1)
 
-# decide how the hidden layers are structured
-hiddenLayers = []
-# # uncomment lines below to get a random number of nodes in the hidden dimensions
-# for dimension in range(data.hiddenDim):
-#     hiddenLayers.append(random.randint(1, 30))
+# Test the trained model
+output = nn.predict(data.testSet)
 
-# create a Neural Network with noOfInputs and noOfOutputs
-nn = NeuralNetwork.NN(data.inputDim, data.outputDim, hiddenLayers)
-
-# train Neural Network
-nn.trainingEpochs(data.trainingSet, TRAINING_STRICTNESS)
-
-print()
-
-# test Neural Network
-nn.testAndPrintResults(data.testSet, TESTING_STRICTNESS)
+print("Predictions after training:")
+nn.print(output, data.testSet)
